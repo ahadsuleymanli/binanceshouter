4e17e57f7bffdbf035caf440277f5a977d30c57b
Ahad Suleymanli <ahad.suleymanli@simbrella.com>
2021-05-21 13:07:24 +0400

    keeps price history with some interval and prints it to console along with the latest price

--- binanceshouter.py
+++ binanceshouter.py
@@ -1,13 +1,15 @@
 import time
 import datetime
+import sys
+from os import system, name
 import requests
 from playsound import playsound
-
+from collections import deque
 
 def check_price(pair: str = "ADAUSDT") -> float:
     response = requests.get(f"https://api.binance.com/api/v3/avgPrice?symbol={pair}")
     price = float(response.json()["price"])
-    print(f"checked price for {pair}: {price}")
+    # print(f"checked price for {pair}: {price}")
     return price
 
 
@@ -17,11 +19,40 @@ def handle_price(price: float, low_limit: float, pair: str) -> None:
 
 def response(price, low_limit, pair):
     playsound('you_suffer.mp3')
-    print(f"{datetime.datetime.now()}: {pair}: price{price}")
+    print(f"{datetime.datetime.now()}: {pair}: price: {price}")
+
+def everynowandthen_generator(every_x_secs: int = 100):
+    secs_passed = -1
+    while True:
+        yield True if (secs_passed := (secs_passed + 1) % every_x_secs) == 0 else False
+
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
 
 if __name__ == "__main__":
+    prices_list = deque(maxlen=10)
+
+    gen = everynowandthen_generator(100)
+
     while True:
         pair = "ADAUSDT"
+        outstring = ""
+        
         price = check_price(pair)
+        if next(gen):
+            prices_list.append(f"{datetime.datetime.now()}: {price}")
+        for x in prices_list:
+            outstring += x + "\n"
+        outstring += f"checked price for {pair}: {price}"
+
+        clear()
+        print(outstring)
+
         handle_price(price, 1.7, pair)
+
         time.sleep(1)
